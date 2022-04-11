746b5816e68ad6b7471cd81fec9456924a4db6c6
sanial2001 <sanial2001@gmail.com>
2022-04-11 21:42:11 +0530

    fixing bugs

--- app/find_theatres.py
+++ app/find_theatres.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import requests
 import json
 import googlemaps
@@ -8,16 +9,17 @@ def find_nearby_theatres(addr):
     API_KEY = f'{settings.API_key}'
     map_client = googlemaps.Client(API_KEY)
 
-    address = addr
-    geocode = map_client.geocode(address=address)
-    (lat, lng) = map(geocode[0]['geometry']['location'].get, ('lat', 'lng'))
-
-    response = map_client.places_nearby(
-        location=(lat, lng),
-        keyword="theatre",
-        type="movie",
-        radius=5000
-    )
-
-    resp = response.get('results')
-    return resp
+    try:
+        address = addr
+        geocode = map_client.geocode(address=address)
+        (lat, lng) = map(geocode[0]['geometry']['location'].get, ('lat', 'lng'))
+        response = map_client.places_nearby(
+            location=(lat, lng),
+            keyword="theatre",
+            type="movie",
+            radius=5000
+        )
+        resp = response.get('results')
+        return resp
+    except:
+        return pd.DataFrame
